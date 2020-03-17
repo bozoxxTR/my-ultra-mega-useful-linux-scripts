46ae3df31aae3f5335ccf960dd90f1316a7ea666
bozoxxTR <31347585+bozoxxTR@users.noreply.github.com>
2020-03-18 00:40:57 +0300

    1

---
+++ hpuefiboot.sh
@@ -0,0 +1,35 @@
+# bozoxx
+# script for elitebook 8460p users
+# hardcoded uefi path fix
+
+
+echo starting
+
+cd ~
+
+sudo mkdir /boot/efi
+
+echo boot path created
+
+# dev/sda# your uefi boot partition
+sudo mount /dev/sda1 /boot/efi
+
+echo boot partition mounted
+
+cd ../../boot/efi/efi
+
+sudo mkdir Microsoft/Boot
+
+cd GRUB
+
+sudo cp grubx64.efi ../Microsoft/Boot/bootmgfw.efi
+
+echo file copied
+
+cd ../../../
+
+sudo umount /boot/efi
+
+echo boot partition unmounted
+
+echo change the world my final message
